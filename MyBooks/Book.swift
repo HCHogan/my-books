6864a6f9c4215f0a417f0190abd7671f649e7f1e
Hank Hogan <ysh2291939848@outlook.com>
2024-02-14 22:36:41 +0800

    Part 1

---
+++ MyBooks/Book.swift
@@ -0,0 +1,69 @@
+//
+//  Book.swift
+//  MyBooks
+//
+//  Created by Hank Hogan on 2024/2/14.
+//
+
+import SwiftUI
+import SwiftData
+
+@Model
+class Book {
+    var title: String
+    var author: String
+    var dateAdded: Date
+    var dateStarted: Date
+    var dateCompleted: Date
+    var summary: String
+    var rating: Int?
+    var status: Status
+    
+    init(
+        title: String,
+        author: String,
+        dateAdded: Date = Date.now,
+        dataStarted: Date = Date.distantPast,
+        dataCompleted: Date = Date.distantPast,
+        summary: String = "",
+        rating: Int? = nil,
+        status: Status = .onShelf
+    ) {
+        self.title = title
+        self.author = author
+        self.dateAdded = dateAdded
+        self.dateStarted = dataStarted
+        self.dateCompleted = dataCompleted
+        self.summary = summary
+        self.rating = rating
+        self.status = status
+    }
+    
+    var icon: Image {
+        switch status {
+        case .onShelf:
+            Image(systemName: "checkmark.diamond.fill")
+        case .inProgress:
+            Image(systemName: "book.fill")
+        case .completed:
+            Image(systemName: "books.vertical.fill")
+        }
+    }
+}
+
+enum Status: Int, Codable, Identifiable, CaseIterable {
+    case onShelf, inProgress, completed
+    var id: Self {
+        self
+    }
+    var descr: String {
+        switch self {
+        case .onShelf:
+            "on Shelf"
+        case .inProgress:
+            "in Progress"
+        case .completed:
+            "completed"
+        }
+    }
+}
---
+++ MyBooks/NewBookView.swift
@@ -0,0 +1,48 @@
+//
+//  NewBookView.swift
+//  MyBooks
+//
+//  Created by Hank Hogan on 2024/2/14.
+//
+
+import SwiftUI
+import SwiftData
+
+struct NewBookView: View {
+    @Environment (\.dismiss) var dismiss
+    @Environment(\.modelContext) private var context
+    
+    @State private var title = ""
+    @State private var author = ""
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                TextField("Book Title", text: $title)
+                TextField("Author", text: $author)
+                Button("Create") {
+                    let newBook = Book(title: title, author: author)
+                    context.insert(newBook)
+                    dismiss()
+                }
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                .buttonStyle(.borderedProminent)
+                .padding(.vertical)
+                .disabled(title.isEmpty || author.isEmpty)
+                .navigationTitle("New Book")
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .topBarLeading) {
+                        Button("Cancel") {
+                            dismiss()
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    NewBookView()
+}

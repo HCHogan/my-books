7891412671670fc394419886cfa034f344142a36
Hank Hogan <ysh2291939848@outlook.com>
2024-02-14 16:48:27 +0800

    Initial Commit

---
+++ MyBooks/MyBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBooksApp.swift
+//  MyBooks
+//
+//  Created by Hank Hogan on 2024/2/14.
+//
+
+import SwiftUI
+
+@main
+struct MyBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
